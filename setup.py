8ce2abc14913bbdc8065c9be9393a9f6fbe3e2d2
roy wang <roy.wang@aspose.com>
2021-11-08 03:42:23 -0600

    Release Aspose.Cells Cloud SDK 21.10

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "asposecellscloud"
-VERSION = "21.9"
+VERSION = "21.10"
 # To install the library, run the following
 #
 # python setup.py install
