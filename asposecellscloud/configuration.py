214d3e26b7ea52fee7455b36a944b54bc850ec3d
roy wang <roy.wang@aspose.com>
2022-09-20 13:59:54 -0500

    20220921 update :add convert apis.

--- asposecellscloud/configuration.py
+++ asposecellscloud/configuration.py
@@ -254,6 +254,6 @@ class Configuration(object):
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 22.8\n"\
-               "SDK Package Version: 22.8".\
+               "Version of the API: 22.9\n"\
+               "SDK Package Version: 22.9".\
                format(env=sys.platform, pyversion=sys.version)
