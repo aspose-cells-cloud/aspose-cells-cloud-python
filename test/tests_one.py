1c05e82ce81a38bc43554b086ca36041cfd67049
roy.wang <roy.wang@aspose.com>
2023-11-05 21:29:09 +0800

    2023-11-05 : Synchronize codes to the origin repository.

--- test/tests_one.py
+++ test/tests_one.py
@@ -18,13 +18,25 @@ from asposecellscloud.requests import *
 
 global_api = None
 
-class TestAutoFilterControllerApi(unittest.TestCase):
+class TestOneCase(unittest.TestCase):
     def setUp(self):
         warnings.simplefilter('ignore', ResourceWarning)
         global global_api
         if global_api is None:
            global_api = CellsApi(AuthUtil.GetClientId(),AuthUtil.GetClientSecret(),"v3.0",AuthUtil.GetBaseUrl())
         self.api = global_api
+    def test_upload_file(self):
+        remote_folder = 'TestData/In'
+
+        local_name = 'Book1.xlsx'
+        remote_name = 'Book1.xlsx'
+
+        mapFiles = { 
+            local_name: os.path.dirname(os.path.realpath(__file__)) + "/../TestData/" +local_name             
+        }
+     
+        request =  UploadFileRequest( mapFiles, remote_folder + '/' + remote_name,storage_name= '')
+        self.api.upload_file(request)
 
     def tearDown(self):
         pass
