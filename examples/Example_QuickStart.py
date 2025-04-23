82b028a5c304654724d10a8a323cab742a56b4b4
roy.wang <roy.wang@aspose.com>
2025-04-23 19:30:09 +0800

    Update code.

--- examples/Example_QuickStart.py
+++ examples/Example_QuickStart.py
@@ -3,9 +3,9 @@ import shutil
 from asposecellscloud.apis.cells_api import CellsApi
 from asposecellscloud.models import *
 from asposecellscloud.requests import *
-CellsCloudClientId ='....'  # get from https://dashboard.aspose.cloud/#/applications
-CellsCloudClientSecret='....'  # get from https://dashboard.aspose.cloud/#/applications
-instance  = CellsApi(CellsCloudClientId,CellsCloudClientSecret)
-# instance  = CellsApi(os.getenv('CellsCloudClientId'),os.getenv('CellsCloudClientSecret'))
+# CellsCloudClientId ='....'  # get from https://dashboard.aspose.cloud/#/applications
+# CellsCloudClientSecret='....'  # get from https://dashboard.aspose.cloud/#/applications
+# instance  = CellsApi(CellsCloudClientId,CellsCloudClientSecret)
+instance  = CellsApi(os.getenv('CellsCloudClientId'),os.getenv('CellsCloudClientSecret'))
 response = instance.put_convert_workbook(PutConvertWorkbookRequest( 'EmployeeSalesSummary.xlsx', 'pdf'))
 shutil.move( response ,"EmployeeSalesSummary.pdf")
